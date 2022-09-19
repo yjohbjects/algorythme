08d620692aba0595187e433badd8ff8270e5516e
yjohbjects <109324483+yjohbjects@users.noreply.github.com>
2022-09-19 14:35:06 +0900

    [Bronze V] Title: 학점계산, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,40 @@
+score = input()
+
+if score == 'A+':
+    print(4.3)
+
+elif score == 'A0':
+    print(4.0)
+
+elif score == 'A-':
+    print(3.7)
+
+elif score == 'B+':
+    print(3.3)
+
+elif score == 'B0':
+    print(3.0)
+
+elif score == 'B-':
+    print(2.7)
+
+elif score == 'C+':
+    print(2.3)
+
+elif score == 'C0':
+    print(2.0)
+
+elif score == 'C-':
+    print(1.7)
+
+elif score == 'D+':
+    print(1.3)
+
+elif score == 'D0':
+    print(1.0)
+
+elif score == 'D-':
+    print(0.7)
+
+elif score == 'F':
+    print(0.0)
