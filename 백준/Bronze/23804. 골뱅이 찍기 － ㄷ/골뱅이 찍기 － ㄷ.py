2973ce03859d01e15f43bec8cc5f13e2da9251b1
yjohbjects <109324483+yjohbjects@users.noreply.github.com>
2022-10-01 01:46:31 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for i in range(N):
+    print('@' * (5 * N))
+
+for i in range(N):
+    for j in range(3):
+        print('@' * N)
+        
+for i in range(N):
+    print('@' * (5 * N))
