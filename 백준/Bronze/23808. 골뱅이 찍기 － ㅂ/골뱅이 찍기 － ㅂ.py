ca9a68fbec199f47748d7eb6b961a35963a0eaa5
yjohbjects <109324483+yjohbjects@users.noreply.github.com>
2022-10-05 23:32:02 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.py
@@ -0,0 +1,13 @@
+N = int(input())
+for i in range(N):
+    print('@' * N + ' ' * 3 * N + '@' * N)
+    print('@' * N + ' ' * 3 * N + '@' * N)
+
+for i in range(N):
+    print('@' * (5 * N))
+
+for i in range(N):
+    print('@' * N + ' ' * 3 * N + '@' * N)
+
+for i in range(N):
+    print('@' * (5 * N))
