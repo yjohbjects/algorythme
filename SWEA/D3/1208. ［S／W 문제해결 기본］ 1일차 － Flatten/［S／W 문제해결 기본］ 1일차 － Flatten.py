d64ee14ee2b486754423b33e69535d28915f9175
yjohbjects <109324483+yjohbjects@users.noreply.github.com>
2022-10-07 20:35:51 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 150 ms, Memory: 61,720 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,25 +1,24 @@
+for t in range(1, 11):
+    N = int(input())
+    nums = list(map(int, input().split()))
 
-for x in range(10):
-    dump = int(input())
-    piles = list(map(int, input().split()))
-
-    for y in range(dump+1):
-        max = 0
-        min = 101
-        highest = 101
-        lowest = 101
-        
-        # min, max 저장
+    dump = 0
+    while dump < N:
+        minV = 101
+        min_idx = 101
+        maxV = 0
+        max_idx = 101
         for i in range(100):
-            if piles[i] >= max:
-                max = piles[i]
-                highest = i
-            if piles[i] < min:
-                min = piles[i]
-                lowest = i
+            if nums[i] > maxV:
+                maxV = nums[i]
+                max_idx = i
 
-        # 1회 덤프
-        piles[highest] -= 1
-        piles[lowest] += 1
+            if nums[i] < minV:
+                minV = nums[i]
+                min_idx = i
 
-    print(f'#{x+1} {max - min}')
+        nums[max_idx] -= 1
+        nums[min_idx] += 1
+        dump += 1
+
+    print(f'#{t} {max(nums) - min(nums)}')
