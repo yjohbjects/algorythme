78d9e04c48a39212fa2860c06c85de37984092de
yjohbjects <109324483+yjohbjects@users.noreply.github.com>
2022-10-24 21:08:21 +0900

    [D2] Title: 파리 퇴치, Time: 140 ms, Memory: 61,468 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,21 +1,21 @@
 T = int(input())
-
-for z in range(1, T+1):
+for t in range(1, T+1):
     N, M = map(int, input().split())
-    paris = [list(map(int, input().split())) for _ in range(N)]
-    paris_num = 0 # 구역별 파리의 합을 임시 저장
-    maxV = 0 # 구역별 파리의 합의 최대값 저장
+    area = [[] for _ in range(N)]
+    for i in range(N):
+        area[i] = list(map(int, input().split()))
+
+
+    maxV = 0
     for i in range(N-M+1):
         for j in range(N-M+1):
+            temp = 0
 
-            # 구역별 파리의 합 구하기
-            # paris_num을 [i][j] 초기화
-            paris_num = 0
             for x in range(M):
                 for y in range(M):
-                    paris_num += paris[i+x][j+y]
+                    temp += area[i+x][j+y]
+            if temp > maxV:
+                maxV = temp
+            # print(temp)
 
-            if paris_num > maxV:
-                maxV = paris_num
-
-    print(f'#{z} {maxV}')
+    print(f'#{t} {maxV}')
