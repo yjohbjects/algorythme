c9793bf86d7c0bc658e6edcc110c31fca0cf7563
yjohbjects <109324483+yjohbjects@users.noreply.github.com>
2022-11-01 20:25:26 +0900

    [Silver II] Title: 주식, Time: 1564 ms, Memory: 356372 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.py
@@ -0,0 +1,18 @@
+# B11501 주식
+# https://www.acmicpc.net/problem/11501
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    price = list(map(int, input().split()))
+
+    answer = 0 # 최대 이익
+    local_max = 0
+    for i in range(N-1, -1, -1):
+        if price[i] > local_max:
+            local_max = price[i]
+
+        if local_max - price[i] > 0:
+            answer += local_max - price[i]
+
+    print(answer)
