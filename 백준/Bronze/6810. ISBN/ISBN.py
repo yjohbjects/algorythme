a8eece802b69a3b23bb62071b201a4817314f2e1
yjohbjects <109324483+yjohbjects@users.noreply.github.com>
2022-11-03 16:49:27 +0900

    [Bronze V] Title: ISBN, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+num = 9 * 1 + 7 * 3 + 8 * 1 + 0 * 3 + 9 * 1 + 2 * 3 + 1 * 1 + 4 * 3 + 1 * 1 + 8 * 3 + a * 1 + b * 3 + c * 1
+
+print(f'The 1-3-sum is {num}')
+
