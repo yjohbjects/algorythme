cb0c251752859016ce635756fdd8633e7ba961cb
YJ <yjohbjects@gmail.com>
2023-04-13 22:36:15 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+    answer = 0
+    
+    numbers = numbers.replace('one', '1')
+    numbers = numbers.replace('two', '2')
+    numbers = numbers.replace('three', '3')
+    numbers = numbers.replace('four', '4')
+    numbers = numbers.replace('five', '5')
+    numbers = numbers.replace('six', '6')
+    numbers = numbers.replace('seven', '7')
+    numbers = numbers.replace('eight', '8')
+    numbers = numbers.replace('nine', '9')
+    numbers = numbers.replace('zero', '0')
+    
+    return int(numbers)
