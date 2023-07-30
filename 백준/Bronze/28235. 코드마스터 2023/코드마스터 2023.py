724b864028a7686b546a96787a5a9447daaf6cb6
YJ <yjohbjects@gmail.com>
2023-07-30 22:50:10 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,13 @@
+word = input()
+
+if word == 'SONGDO':
+    print('HIGHSCHOOL')
+    
+elif word == 'CODE':
+    print('MASTER')
+    
+elif word == '2023':
+    print('0611')
+    
+else:
+    print('CONTEST')
