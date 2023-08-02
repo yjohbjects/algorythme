2a31bfc879067a9581e85dc6a915e15259ce3149
YJ <yjohbjects@gmail.com>
2023-08-02 22:45:55 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+acro = input()
+
+if acro == 'NLCS':
+    print('North London Collegiate School')
+elif acro == 'BHA':
+    print('Branksome Hall Asia')
+elif acro == 'KIS':
+    print('Korea International School')
+elif acro == 'SJA':
+    print('St. Johnsbury Academy')
