536139d99aee1e0f735a217cca78bf2fcbda6ae9
YJ <yjohbjects@gmail.com>
2023-09-03 23:53:00 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+letter = input()
+
+if letter == 'M':
+    print('MatKor')
+elif letter == 'W':
+    print('WiCys')
+elif letter == 'C':
+    print('CyKor')
+elif letter == 'A':
+    print('AlKor')
+else:
+    print('$clear')
