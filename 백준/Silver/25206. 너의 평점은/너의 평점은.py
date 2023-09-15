f92491cb7ffba369388c6f8dce1e4246133a270b
YJ <yjohbjects@gmail.com>
2023-09-16 00:40:34 +0900

    [Silver V] Title: 너의 평점은, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,21 @@
+credits = 0
+summation = 0
+calc = {'A+':4.5,
+        'A0':4.0,
+        'B+':3.5,
+        'B0':3.0,
+        'C+':2.5,
+        'C0':2.0,
+        'D+':1.5,
+        'D0':1.0,
+        'F':0
+        }
+
+for _ in range(20):
+    subject, credit, grade = map(str, input().split())
+
+    if grade != 'P':
+        credits += float(credit)
+        summation += float(credit) * calc[grade]
+
+print(summation/credits)
