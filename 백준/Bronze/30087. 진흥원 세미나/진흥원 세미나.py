a82f63b748c0a6a0988b7ecbb2f8b1b5b5877775
YJ <yjohbjects@gmail.com>
2023-10-25 12:48:24 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,15 @@
+seminar = {
+    "Algorithm": "204",
+    "DataAnalysis": "207",
+    "ArtificialIntelligence": "302",
+    "CyberSecurity": "B101",
+    "Network": "303",
+    "Startup": "501",
+    "TestStrategy": "105",
+}
+
+N = int(input())
+
+for _ in range(N):
+    L = input()
+    print(seminar[L])
